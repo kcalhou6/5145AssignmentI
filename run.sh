0bf382d6704976c5f3bd37a5ddd3bde33fa09274
kcalhou6 <kcalhou6@uncc.edu>
2016-09-06 19:46:21 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-g++ assignmentI.cpp -o assignmentI -O3 -march=native
+g++ assignmentI.cpp -o assignmentI -O3 -march=native -std=gnu++0x
 ./assignmentI 10000 10000
 ./assignmentI 100000 10000
 ./assignmentI 1000000 10000
