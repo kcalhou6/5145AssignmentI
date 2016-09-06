cc318eddae333d517bf9b941dd0f9abb35f1dd3e
KC <kcalhou6@uncc.edu>
2016-09-06 19:39:28 -0400

    4

--- assignmentI.cpp
+++ assignmentI.cpp
@@ -6,50 +6,73 @@
 
 using namespace std;
 
+//Each node of the linked list will be represented by the class LLInt
 class LLInt {
+	//Instance variables value to hold the integer value of the node
+	//and *next to hold a pointer to the next node in the linked list
 	private:
 		int value;
 		LLInt *next;
 
 	public:
+		//Default constructor, sets value to be null and *next to be the
+		//null pointer
 		LLInt() {
 			this->value = NULL;
 			this->next = nullptr;
 		}
+		//Overloaded constructor which takes a parameter for the value,
+		//sets value to the provided value and *next to be the null pointer
 		LLInt(int value) {
 			this->value = value;
 			this->next = nullptr;
 		}
+		//Overloaded constructor which takes parameters for the value and
+		//next pointer, sets value to the provided value and *next to the
+		//provided pointer
 		LLInt(int value, LLInt * next) {
 			this->value = value;
 			this->next = next;
 		}
+		//Helper method which returns the integer value
 		int getValue() {
 			return value;
 		}
+		//Helper method which returns the next node pointer
 		LLInt * getNext() {
 			return next;
 		}
 };
 
+//Class to provided linked list implementation
 class LinkedList {
+	//Instance variable *head to hold a pointer to the first node in the
+	//linked list
 	private:
 		LLInt *head;
-		int size;
 		
 	public:
+		//Default constructor for an empty linked list, sets *head to
+		//the null pointer
 		LinkedList() {
 			head = nullptr;
 		}
+		//Helper method which takes a parameter for value and creates
+		//a new node for the linkedList with the provided value and
+		//pointer to the current head node then inserts it into the linked
+		//list by marking it as the head node. 
 		void insert(int value) {
 			LLInt *l = new LLInt(value, head);
 			head = l;
 		}
+		//Helper method which returns the head pointer
 		LLInt * getHead() {
 			return head;
 		}
-		//returns pointer to int val if val is found in the list
-		//or -1 if the val is not found
+		//Helper method which take a parameter value and searches for a
+		//node in the linked list with the provided value. Returns a
+		//pointer to the LLInt if val is found in the list or nullptr if the
+		//val is not found
 		LLInt * find(int val) {
 			LLInt *current = head;
 			while(current->getNext() != NULL) {
@@ -65,12 +88,16 @@ class LinkedList {
 
 int main(int argc, char *argv[])
 {
+	//Create an initially empty linked list
 	LinkedList lL;
 	
+	//Setup a text file for output of search times corresponding to linked list
+	//size
 	ofstream statFile;
 	statFile.open ("stats.txt", ios::app);
 	
-	//Insert argv[1] # of random integers into the linked list
+	//Insert command line argument #1 number of random integers into the
+	//linked list with values in the range 0-1000000000000
 	for (int a = 0; a < atoi(argv[1]); a++) {
 		lL.insert(rand() % 1000000000000);
 	}
@@ -79,6 +106,10 @@ int main(int argc, char *argv[])
 	int search;
 	//timeval start, end;
 	timespec start, end;
+	
+	//Search for command line argument #2 number of random integers in the linked
+	//list with values in a range 0-1000000000000000, and write the time each
+	//search takes to the stats file
 	for (int a = 0; a < atoi(argv[2]); a++) {
 		search = (rand() % 1000000000000000);
 		clock_gettime(CLOCK_REALTIME, &start);
