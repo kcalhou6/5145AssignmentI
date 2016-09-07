58227a0de0a29ea98a0151c8958c3908eb16fc35
kcalhou6 <kcalhou6@mba-i1.uncc.edu>
2016-09-06 21:46:55 -0400

    .

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 g++ assignmentI.cpp -o assignmentI -O3 -march=native -std=gnu++0x
-./assignmentI 10000 10000
-./assignmentI 100000 10000
-./assignmentI 1000000 10000
-./assignmentI 10000000 10000
-./assignmentI 100000000 10000
-./assignmentI 1000000000 10000
+./assignmentI 10000 100
+./assignmentI 100000 100
+./assignmentI 1000000 100
+./assignmentI 10000000 100
+./assignmentI 100000000 100
+./assignmentI 1000000000 100
